214200a5e5fd9281d9cffd33df3408bc0b3b7d5c
gerardl <tad284@gmail.com>
2016-11-08 23:26:21 -0600

    add remote repo location

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,9 +1,12 @@
 (function () {
     'use strict';
     var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+    var socket;
 
     function preload () {};
-    function create () {};
+    function create () {
+        socket = io.connect();
+    };
     function update () {};
     function render () {};
 })();
